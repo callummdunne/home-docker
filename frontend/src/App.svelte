df0a00dab9d61be176dc30eff8d164eedd87805a
Callum Dunne <callum@bbd.co.za>
2021-10-24 22:59:23 +0200

    feat(frontend): Made some styling changes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,39 +1,78 @@
 <script lang="ts">
   const base = "http://localhost:";
   export const mappings = [
-    { id: "pihole", route: `${base}81`, imageLink: "assets/pihole.svg" },
-    { id: "plex", route: `${base}32400/web`, imageLink: "assets/plex.svg" },
-    { id: "radarr", route: `${base}7878`, imageLink: "assets/radarr.svg" },
-    { id: "jackett", route: `${base}9117`, imageLink: "assets/jackett.svg" },
-    { id: "sonarr", route: `${base}8989`, imageLink: "assets/sonarr.svg" },
-    { id: "deluge", route: `${base}8112`, imageLink: "assets/deluge.svg" },
+    {
+      id: "pihole",
+      route: `${base}81`,
+      imageLink: "assets/pihole.svg",
+      name: "Pihole",
+    },
+    {
+      id: "plex",
+      route: `${base}32400/web`,
+      imageLink: "assets/plex.svg",
+      name: "Plex",
+    },
+    {
+      id: "radarr",
+      route: `${base}7878`,
+      imageLink: "assets/radarr.svg",
+      name: "Radarr",
+    },
+    {
+      id: "jackett",
+      route: `${base}9117`,
+      imageLink: "assets/jackett.svg",
+      name: "Jackett",
+    },
+    {
+      id: "sonarr",
+      route: `${base}8989`,
+      imageLink: "assets/sonarr.svg",
+      name: "Sonarr",
+    },
+    {
+      id: "deluge",
+      route: `${base}8112`,
+      imageLink: "assets/deluge.svg",
+      name: "Deluge",
+    },
   ];
 </script>
 
 <main>
-  <h1>Welcome to your Home-Docker</h1>
+  <h1 class="top-heading">Welcome to your Home-Docker</h1>
   <div class="grid">
-    {#each mappings as { id, route, imageLink }}
-      <div />
-      <img src={imageLink} alt={id} />
-      <!-- <iframe src={route} title={id} /> -->
+    {#each mappings as { id, route, imageLink, name }}
+      <div class="card">
+        <h1>{name}</h1>
+        <a href={route}>
+          <img class="image" src={imageLink} alt={id} />
+        </a>
+      </div>
     {/each}
   </div>
 </main>
 
 <style>
+  :root {
+    --background: #232931;
+    --standout: #3d4750;
+    --contrast: #4ecca3;
+    --notSelected: #393e46;
+    --whiteish: #eeeeee;
+  }
   main {
+    background: var(--background);
     text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-    height: 100%;
-    width: 100%;
     display: flex;
     flex-direction: column;
+    max-height: 100%;
+    overflow: auto;
   }
 
-  h1 {
-    color: #ff3e00;
+  .top-heading {
+    color: var(--contrast);
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
@@ -46,11 +85,25 @@
   }
 
   .grid {
-    height: 100%;
+    height: 80%;
     display: grid;
-    grid-template-columns: auto auto auto;
-    grid-template-rows: auto auto;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
     column-gap: 10px;
     row-gap: 15px;
+  }
+
+  .image {
+    height: 100%;
+    width: 100%;
+  }
+  .card {
+    background: var(--whiteish);
+    border-radius: 5%;
+    display: flex;
+    flex-direction: column;
+  }
+  .card:hover h1 {
+    color: var(--contrast);
   }
 </style>
